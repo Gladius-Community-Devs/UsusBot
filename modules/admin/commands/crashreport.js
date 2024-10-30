5a4262d1ce15e8796478335b4236ba6d701f5fa4
JR <jrbuda01@gmail.com>
2024-10-30 11:45:20 -0700

    Adding a command to be able to help diagnose crash reports

--- modules/admin/commands/crashreport.js
+++ modules/admin/commands/crashreport.js
@@ -11,6 +11,37 @@ module.exports = {
             message.channel.send({ content: "You do not have permission to use this command." });
             return;
         }
-        //const log_wipe = spawn("find ~/ModBot/logs -mtime +1 -exec rm {} \;");
+        try {
+            const logsDir = path.join(__dirname, '../../logs');
+            const files = fs.readdirSync(logsDir)
+                .filter(file => file.endsWith('.log'))
+                .map(file => ({ file, time: fs.statSync(path.join(logsDir, file)).mtime }))
+                .sort((a, b) => b.time - a.time);
+
+            if (files.length < 2) {
+                message.channel.send({ content: 'Not enough log files to tail the second most recent one.' });
+                return;
+            }
+
+            const secondLatestLogFile = path.join(logsDir, files[1].file);
+            const logContent = fs.readFileSync(secondLatestLogFile, 'utf8');
+
+            const filteredLines = logContent.split('\n').filter(line => !line.startsWith('level'));
+            const output = filteredLines.join('\n');
+
+            if (output.length === 0) {
+                message.channel.send({ content: 'No relevant lines found in the log file.' });
+            } else if (output.length > 2000) {
+                message.channel.send({ content: 'The log output is too long to send in a single message. Please check the file directly.' });
+            } else {
+                message.channel.send({ content: `\`\`\`${output}\`\`\`` });
+            }
+
+            // Attach the log file
+            await message.channel.send({ files: [secondLatestLogFile] });
+        } catch (error) {
+            console.error('Error reading log file:', error);
+            message.channel.send({ content: 'An error occurred while trying to read the log file.' });
+        }
     }
 };
