28378540ef8de271b500283ccb930ae9b5b16af7
JR <jrbuda01@gmail.com>
2024-10-30 01:58:14 -0700

    Had some config issues, Usus can use his DB now!

--- modules/gladius/commands/ping.js
+++ modules/gladius/commands/ping.js
@@ -1,12 +1,12 @@
 module.exports = {
-    name: 'ping',
-    description: 'Sends a message back. Used to test if the bot is working.',
+    name: 'hello',
+    description: 'Say hi to Usus! Used to test if the bot is working.',
     syntax: 'ping [arbitrary argument for testing]',
     num_args: 0,
     args_to_lower: true,
     needs_api: false,
     has_state: false,
     execute(message, args, extra) {
-      message.channel.send({ content: "Gladius Pong!"});
+      message.channel.send({ content: "Hello there! Ready to participate in the games?"});
     }
 };
