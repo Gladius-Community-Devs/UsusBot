0ffbb654f85bb610780c9f61b59a2218835feec1
JR <jrbuda01@gmail.com>
2024-10-30 22:59:32 -0700

    Updated help command to not show the module, not needed

--- modules/core/commands/help.js
+++ modules/core/commands/help.js
@@ -1,23 +1,15 @@
 module.exports = {
     name: 'help',
-    description: 'Provides a list of commands, optionally just the ones from a specific module',
-    syntax: 'help [module_name]',
+    description: 'Provides a list of commands',
+    syntax: 'help',
     num_args: 0,
     args_to_lower: false,
     needs_api: true,
     has_state: false,
     async execute(message, args, extra) {
-        var api = extra.api;
         var mod_handler = extra.module_handler;
 
-        // Check if the user has the 'Admin' role
-        if (!message.member.roles.cache.some(role => role.name === 'Admin')) {
-            message.channel.send({ content: "You do not have permission to use this command." });
-            return;
-        }
-
         var longest_syntax = "";
-        var longest_module_name = "";
         for (var current_module_name of Array.from(mod_handler.modules.keys())) {
             var current_module = mod_handler.modules.get(current_module_name);
 
@@ -26,125 +18,48 @@ module.exports = {
                 if (current_command.syntax.length > longest_syntax.length) {
                     longest_syntax = current_command.syntax;
                 }
-                if (current_module.config.display_name.length > longest_module_name.length) {
-                    longest_module_name = current_module.config.display_name;
-                }
             }
         }
 
         var header_syntax_length = Math.max(longest_syntax.length, 7); // "Command" length
-        var header_module_length = Math.max(longest_module_name.length, 6); // "Module" length
-        var desc_space = Math.max(50, 80 - header_syntax_length - header_module_length);
+        var desc_space = Math.max(50, 115 - header_syntax_length);
 
         var output = '```';
-        output += `Command${" ".repeat(header_syntax_length - 7)} | Module${" ".repeat(header_module_length - 6)} | Description
+        output += `Command${" ".repeat(header_syntax_length - 7)} | Description
 `;
-        output += "-".repeat(header_syntax_length) + "-+-" + "-".repeat(header_module_length) + "-+-" + "-".repeat(desc_space) + "\n";
+        output += "-".repeat(header_syntax_length) + "-+-" + "-".repeat(desc_space) + "\n";
 
-        if(args.length > 1) {
-            var module_name = args[1];
-            
-            var respGetModule = await api.get('module', {
-                name: module_name
-            });
+        var num_lines = 0;
 
-            if(respGetModule.modules.length <= 0) {
-                message.channel.send({ content: "Sorry, that module doesn't exist in the server-specific registries!"});
-                return;
-            }
+        for (var current_module_name of Array.from(mod_handler.modules.keys())) {
+            var current_module = mod_handler.modules.get(current_module_name);
+            for (var current_command_name of Array.from(current_module.commands.keys())) {
+                var current_command = current_module.commands.get(current_command_name);
 
-            var respModEnabled = await api.get('enabled_module', {
-                module_id: parseInt(respGetModule.modules[0].module_id),
-                server_id: message.guild.id
-            });
-            
-            if(mod_handler.modules.has(module_name) && respModEnabled.enabled_modules.length > 0) {
-                var selected_module = mod_handler.modules.get(module_name);
-                var num_lines = 0;
+                output += current_command.syntax + " ".repeat(header_syntax_length - current_command.syntax.length);
+                output += " | ";
 
-                for(var current_command_name of Array.from(selected_module.commands.keys())) {
-                    var current_command = selected_module.commands.get(current_command_name);
-
-                    output += current_command.syntax + " ".repeat(header_syntax_length - current_command.syntax.length);
-                    output += " | " + selected_module.config.display_name + " ".repeat(header_module_length - selected_module.config.display_name.length);
-                    output += " | ";
-
-                    if(current_command.description.length > desc_space) {
-                        output += current_command.description.substring(0, desc_space - 3) + "...";
-                    } else {
-                        output += current_command.description;
-                    }
-
-                    output += "\n";
-                    num_lines++;
-
-                    if(num_lines >= 14) {
-                        output += "```";
-                        message.channel.send({ content: output});
-                        output = "```";
-                        output += `Command${" ".repeat(header_syntax_length - 7)} | Module${" ".repeat(header_module_length - 6)} | Description
-`;
-                        output += "-".repeat(header_syntax_length) + "-+-" + "-".repeat(header_module_length) + "-+-" + "-".repeat(desc_space) + "\n";
-                        num_lines = 0;
-                    }
-                }
-                output += "```";
-                message.channel.send({ content: output});
-            } else {
-                message.channel.send({ content: "Sorry, I couldn't find that module!"});
-            }
-        } else {
-            var num_lines = 0;
-
-            for(var current_module_name of Array.from(mod_handler.modules.keys())) {
-                var current_module = mod_handler.modules.get(current_module_name);
-                if(current_module.config.display_name == "Core" || current_module.config.display_name == "Admin") {
-                    continue;
-                }
-                var respModule = await api.get('module', {
-                    name: current_module_name
-                });
-
-                if(respModule.modules.length <= 0) {
-                    message.channel.send({ content: "Oops, something went wrong!"});
-                    return;
+                if (current_command.description.length > desc_space) {
+                    output += current_command.description.substring(0, desc_space - 3) + "...";
+                } else {
+                    output += current_command.description;
                 }
 
-                var respModEnabled = await api.get('enabled_module', {
-                    module_id: parseInt(respModule.modules[0].module_id)
-                });
+                output += "\n";
 
-                if(respModEnabled.enabled_modules.length > 0) {
-                    for(var current_command_name of Array.from(current_module.commands.keys())) {
-                        var current_command = current_module.commands.get(current_command_name);
-
-                        output += current_command.syntax + " ".repeat(header_syntax_length - current_command.syntax.length);
-                        output += " | " + current_module.config.display_name + " ".repeat(header_module_length - current_module.config.display_name.length);
-                        output += " | ";
-
-                        if(current_command.description.length > desc_space) {
-                            output += current_command.description.substring(0, desc_space - 3) + "...";
-                        } else {
-                            output += current_command.description;
-                        }
-
-                        output += "\n";
-
-                        num_lines++;
-                        if(num_lines >= 14) {
-                            output += "```";
-                            message.channel.send({ content: output});
-                            output = "```";
-                            output += `Command${" ".repeat(header_syntax_length - 7)} | Module${" ".repeat(header_module_length - 6)} | Description
+                num_lines++;
+                if (num_lines >= 14) {
+                    output += "```";
+                    message.channel.send({ content: output });
+                    output = "```";
+                    output += `Command${" ".repeat(header_syntax_length - 7)} | Description
 `;
-                            output += "-".repeat(header_syntax_length) + "-+-" + "-".repeat(header_module_length) + "-+-" + "-".repeat(desc_space) + "\n";
-                            num_lines = 0;
-                        }
-                    }
+                    output += "-".repeat(header_syntax_length) + "-+-" + "-".repeat(desc_space) + "\n";
+                    num_lines = 0;
                 }
             }
-            output += "```";
-            message.channel.send({ content: output});
         }
+        output += "```";
+        message.channel.send({ content: output });
     }
 };
