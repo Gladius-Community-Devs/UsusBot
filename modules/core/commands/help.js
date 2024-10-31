8afdcb710bb12e3183f59ab6198dfb69763eade6
JR <jrbuda01@gmail.com>
2024-10-30 23:18:06 -0700

    Updated help command to not show the module, not needed

--- modules/core/commands/help.js
+++ modules/core/commands/help.js
@@ -1,3 +1,5 @@
+const { EmbedBuilder } = require('discord.js');
+
 module.exports = {
     name: 'help',
     description: 'Provides a list of commands',
@@ -8,26 +10,11 @@ module.exports = {
     has_state: false,
     async execute(message, args, extra) {
         var mod_handler = extra.module_handler;
-
-        var longest_syntax = "";
         var gladius_module = mod_handler.modules.get('gladius');
 
-        for (var current_command_name of Array.from(gladius_module.commands.keys())) {
-            var current_command = gladius_module.commands.get(current_command_name);
-            if (current_command.syntax.length > longest_syntax.length) {
-                longest_syntax = current_command.syntax;
-            }
-        }
-
-        var header_syntax_length = Math.max(longest_syntax.length, 40); // Set to fit within display width
-        var header_desc_length = 85; // Remaining space for description based on total width
-
-        var output = '```';
-        output += `Command (o) = optional${" ".repeat(header_syntax_length - 22)} | Description
-`;
-        output += "-".repeat(header_syntax_length) + "-+-" + "-".repeat(header_desc_length) + "\n";
-
-        var num_lines = 0;
+        const embed = new EmbedBuilder()
+            .setTitle('Help - List of Commands')
+            .setColor(0x00AE86);
 
         for (var current_command_name of Array.from(gladius_module.commands.keys())) {
             var current_command = gladius_module.commands.get(current_command_name);
@@ -36,29 +23,13 @@ module.exports = {
                 continue;
             }
 
-            output += current_command.syntax + " ".repeat(header_syntax_length - current_command.syntax.length);
-            output += " | ";
-
-            if (current_command.description.length > header_desc_length) {
-                output += current_command.description.substring(0, header_desc_length - 3) + "...";
-            } else {
-                output += current_command.description;
-            }
-
-            output += "\n";
-
-            num_lines++;
-            if (num_lines >= 14) {
-                output += "```";
-                message.channel.send({ content: output });
-                output = "```";
-                output += `Command (o) = optional${" ".repeat(header_syntax_length - 20)} | Description
-`;
-                output += "-".repeat(header_syntax_length) + "-+-" + "-".repeat(header_desc_length) + "\n";
-                num_lines = 0;
-            }
+            embed.addFields({
+                name: current_command.syntax,
+                value: current_command.description.length > 85 ? current_command.description.substring(0, 82) + '...' : current_command.description,
+                inline: true
+            });
         }
-        output += "```";
-        message.channel.send({ content: output });
+
+        message.channel.send({ embeds: [embed] });
     }
 };
